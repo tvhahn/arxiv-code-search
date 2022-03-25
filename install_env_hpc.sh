4e5fea0d523f674bc63dfe3227ae2234e0931a7f
tvhahn <tvhahn@gmail.com>
2022-03-25 08:55:09 -0700

    Update

--- install_env_hpc.sh
+++ install_env_hpc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 module load python/3.8
-virtualenv ~/doiscrape
-source ~/doiscrape/bin/activate
+virtualenv ~/arxiv
+source ~/arxiv/bin/activate
 pip install --upgrade pip setuptools wheel
-pip install --no-index pandas scipy scikit_learn matplotlib seaborn torch torchvision
+pip install --no-index pandas scipy scikit_learn matplotlib seaborn
 pip install --no-index jupyterlab requests beautifulsoup4 lxml black tqdm
-pip install pdfminer.six
+pip install pdfminer.six kaggle
 pip install -e .
 
 # create bash script for opening jupyter notebooks https://stackoverflow.com/a/4879146/9214620
