e148256b44e4a9ec7adce9b65622b5b5b08dc7bb
tvhahn <tvhahn@gmail.com>
2022-04-13 11:45:47 -0400

    16G for sbatch

--- src/data/parse_json.sh
+++ src/data/parse_json.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=00:30:00 # 30 minutes
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=16G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
