3bd0b73207b335091ba452f9978c00de044889ec
tvhahn <tvhahn@gmail.com>
2022-05-11 14:23:02 -0700

    add early stopping class to utils.py

--- src/models/utils.py
+++ src/models/utils.py
@@ -62,3 +62,85 @@ def create_data_loader(df, tokenizer, max_len, batch_size):
     batch_size=batch_size,
     num_workers=4
   )
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+
+    def __init__(
+        self,
+        patience=7,
+        verbose=False,
+        delta=0.005,
+        path="checkpoint.pt",
+        trace_func=print,
+        early_stop_delay=20,
+    ):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print      
+
+        From https://github.com/Bjarten/early-stopping-pytorch
+        License: MIT     
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.early_stop_delay = early_stop_delay
+        self.counter = 0
+        self.epoch = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+        # print(type(score), 'SCORE ####,', score)
+
+        if self.epoch < self.early_stop_delay:
+            self.epoch += 1
+            pass
+        else:
+            if self.best_score is None:
+                self.best_score = score
+                self.save_checkpoint(val_loss, model)
+            elif score < self.best_score + self.delta:
+                self.counter += 1
+                if self.verbose:
+                    self.trace_func(
+                        f"EarlyStopping counter: {self.counter} out of {self.patience}"
+                    )
+                if self.counter >= self.patience:
+                    self.early_stop = True
+            elif torch.isnan(score).item():
+                self.counter += 1
+                if self.counter >= self.patience:
+                    self.early_stop = True
+                # print('########## IS NAN #######')
+            else:
+                self.best_score = score
+                self.save_checkpoint(val_loss, model)
+                self.counter = 0
+            self.epoch += 1
+
+    def save_checkpoint(self, val_loss, model):
+        """Saves model when validation loss decrease."""
+        if self.verbose:
+            self.trace_func(
+                f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ..."
+            )
+        torch.save(model, self.path)
+        # torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
