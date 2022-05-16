100d3ef0728df0176829585b1b66e9aec760d564
tvhahn <tvhahn@gmail.com>
2022-05-16 15:48:50 -0700

    added extra layer to model

--- src/models/model.py
+++ src/models/model.py
@@ -8,13 +8,15 @@ class ArxivClassifier(nn.Module):
         super(ArxivClassifier, self).__init__()
         self.encoder = BertModel.from_pretrained(pre_trained_model_name)
         self.dense_1 = torch.nn.Linear(768, 768)
-        self.dropout = torch.nn.Dropout(0.3)
+        self.dropout = torch.nn.Dropout(0.2)
         self.dense_2 = torch.nn.Linear(768, n_classes)
 
     def forward(self, input_ids, attention_mask):
         output_1 = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
         hidden_state = output_1[0]
         pooled_output = hidden_state[:, 0]
+        pooled_output = self.dense_1(pooled_output)
+        pooled_output = torch.nn.ReLU()(pooled_output)
         pooled_output = self.dense_1(pooled_output)
         pooled_output = torch.nn.ReLU()(pooled_output)
         pooled_output = self.dropout(pooled_output)
