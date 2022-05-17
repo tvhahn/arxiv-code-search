e800463a429864d32a6e467fd07c46ba18ca95f2
tvhahn <tvhahn@gmail.com>
2022-05-16 18:46:19 -0700

    update

--- src/models/model.py
+++ src/models/model.py
@@ -8,7 +8,7 @@ class ArxivClassifier(nn.Module):
         super(ArxivClassifier, self).__init__()
         self.encoder = BertModel.from_pretrained(pre_trained_model_name)
         self.dense_1 = torch.nn.Linear(768, 768)
-        self.dropout = torch.nn.Dropout(0.2)
+        self.dropout = torch.nn.Dropout(0.5)
         self.dense_2 = torch.nn.Linear(768, n_classes)
 
     def forward(self, input_ids, attention_mask):
@@ -17,8 +17,8 @@ class ArxivClassifier(nn.Module):
         pooled_output = hidden_state[:, 0]
         pooled_output = self.dense_1(pooled_output)
         pooled_output = torch.nn.ReLU()(pooled_output)
-        pooled_output = self.dense_1(pooled_output)
-        pooled_output = torch.nn.ReLU()(pooled_output)
+        # pooled_output = self.dense_1(pooled_output)
+        # pooled_output = torch.nn.ReLU()(pooled_output)
         pooled_output = self.dropout(pooled_output)
         output = self.dense_2(pooled_output)
         return output
