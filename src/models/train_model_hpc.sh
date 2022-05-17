bca11f6daf283db7d66cd80e88f946b336d9df02
tvhahn <tvhahn@gmail.com>
2022-05-17 11:31:26 -0700

    update

--- src/models/train_model_hpc.sh
+++ src/models/train_model_hpc.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1       # request GPU "generic resource"
 #SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=8G      # memory per node
-#SBATCH --time=0-00:20      # time (DD-HH:MM)
+#SBATCH --time=0-01:20      # time (DD-HH:MM)
 #SBATCH --job-name=_test
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
@@ -32,6 +32,6 @@ python $PROJECT_DIR/src/models/train_model.py \
     --path_model_dir $SCRATCH_DIR/models \
     --batch_size 8 \
     --n_epochs 1000 \
-    --n_classes 4 \
+    --n_classes 2 \
     # --model_time_suffix var4 \
     # --checkpoint  2021_11_18_103508_var4 \
