8da1941ed80ed1215d9d7bfef201828dad490c0d
tvhahn <tvhahn@gmail.com>
2022-06-03 20:41:32 -0700

    Setup for hpc

--- src/models_classical/train_hpc.sh
+++ src/models_classical/train_hpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=00:5:00 # 30 min
-#SBATCH --array=1-20
+#SBATCH --time=00:05:00 # 30 min
+#SBATCH --array=1-2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
 #SBATCH --account=rrg-mechefsk
@@ -11,10 +11,10 @@ PROJECT_DIR=$1
 
 NOW_TIME=$2
 
-SCRATCH_DATA_DIR=~/scratch/feat-store/data
+SCRATCH_DATA_DIR=~/scratch/arxiv-code-search/data
 
 module load python/3.8
-source ~/featstore/bin/activate
+source ~/arxiv/bin/activate
 
-python $PROJECT_DIR/src/models/train.py --save_dir_name interim_results_milling --date_time $NOW_TIME --path_data_dir $SCRATCH_DATA_DIR --rand_search_iter 300 --feat_selection
+python $PROJECT_DIR/src/models/train.py --save_dir_name interim_classical_results --date_time $NOW_TIME --path_data_dir $SCRATCH_DATA_DIR --rand_search_iter 300
 
