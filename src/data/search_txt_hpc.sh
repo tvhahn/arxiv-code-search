bf16ebaa9ebc224df83fbd6be9b3e66bc5e9d3aa
tvhahn <tvhahn@gmail.com>
2022-07-06 12:21:21 -0400

    Update slurm scripts

--- src/data/search_txt_hpc.sh
+++ src/data/search_txt_hpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:10:00 # 30 minutes
+#SBATCH --time=00:20:00 # 30 minutes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=4G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
@@ -11,4 +11,8 @@ module load python/3.8
 
 source ~/arxiv/bin/activate
 
-python src/data/search_txt.py --txt_root_dir /home/tvhahn/scratch/arxiv-code-search/raw/txts --overwrite --keep_old_files
+python src/data/search_txt.py --txt_root_dir /home/tvhahn/scratch/arxiv-code-search/data/raw/txts \
+    --overwrite \
+    --keep_old_files \
+    --index_file_no 5 \
+    --max_token_len 350
