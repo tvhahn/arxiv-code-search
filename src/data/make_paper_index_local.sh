319e4cf32a2b97b7c2ffaa937b1e4e7e6ffa2b83
tvhahn <tvhahn@gmail.com>
2022-07-05 20:08:38 -0700

    Create new index file

--- src/data/make_paper_index_local.sh
+++ src/data/make_paper_index_local.sh
@@ -4,8 +4,8 @@ PROJECT_DIR=$1
 
 python src/data/make_paper_index.py \
     --metadata_name "metadata_subsample_50k.csv" \
-    --n_papers 100 \
+    --n_papers 500 \
     --license_filter_list "['CC BY 4.0', 'CC0 1.0', 'CC']" \
-    --regex_pattern_cat "\beess|\bcs" \
+    --regex_pattern_cat "\beess|\bcs|\bastro-ph|\bstat|\bphysics" \
     # --start_date "2020-01-01" \
     # --end_date "2020-12-31"
