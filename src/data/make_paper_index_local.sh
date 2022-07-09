51b6815f5af7a838f3d4368f159c44a3bedbb37d
tvhahn <tvhahn@gmail.com>
2022-07-08 18:05:21 -0700

    Update

--- src/data/make_paper_index_local.sh
+++ src/data/make_paper_index_local.sh
@@ -7,5 +7,5 @@ python src/data/make_paper_index.py \
     --n_papers 500 \
     --license_filter_list "['CC BY 4.0', 'CC0 1.0', 'CC']" \
     --regex_pattern_cat "\beess|\bcs|\bastro-ph|\bstat|\bphysics" \
-    # --start_date "2020-01-01" \
-    # --end_date "2020-12-31"
+    --start_date "2015-01-01" \
+    --end_date "2019-12-31"
