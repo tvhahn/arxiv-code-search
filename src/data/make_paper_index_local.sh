4ccc414e9bdb786a2b80a62871c8435f8a7dc181
tvhahn <tvhahn@gmail.com>
2022-07-08 22:45:30 -0700

    New index file

--- src/data/make_paper_index_local.sh
+++ src/data/make_paper_index_local.sh
@@ -6,6 +6,6 @@ python src/data/make_paper_index.py \
     --metadata_name "metadata_subsample_50k.csv" \
     --n_papers 500 \
     --license_filter_list "['CC BY 4.0', 'CC0 1.0', 'CC']" \
-    --regex_pattern_cat "\beess|\bcs|\bastro-ph|\bstat|\bphysics" \
-    --start_date "2015-01-01" \
-    --end_date "2019-12-31"
+    --regex_pattern_cat "\beess|\bcs|\bastro-ph|\bstat|\bphysics" 
+    # --start_date "2015-01-01" \
+    # --end_date "2019-12-31"
