f4a18dcc71be247b557965d22169202a24a20606
tvhahn <tvhahn@gmail.com>
2022-07-14 14:54:03 -0400

    Change formatting

--- src/models_classical/compile_hpc.sh
+++ src/models_classical/compile_hpc.sh
@@ -14,5 +14,9 @@ SCRATCH_DATA_DIR=~/scratch/arxiv-code-search/data
 module load python/3.8
 source ~/arxiv/bin/activate
 
-python $PROJECT_DIR/src/models_classical/compile.py -p $PROJECT_DIR --n_cores 6 --interim_dir_name classical_results_interim --final_dir_name final_results_classical
+python $PROJECT_DIR/src/models_classical/compile.py \
+    -p $PROJECT_DIR \
+    --n_cores 6 \
+    --interim_dir_name classical_results_interim_2022-07-14 \
+    --final_dir_name final_results_classical_2022-07-14
 
