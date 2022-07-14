73da6f28a5f05ad042e54f8d6e7440c3098fc15d
tvhahn <tvhahn@gmail.com>
2022-07-14 11:32:12 -0700

    Update

--- src/models_classical/train_hpc.sh
+++ src/models_classical/train_hpc.sh
@@ -17,9 +17,9 @@ module load python/3.8
 source ~/arxiv/bin/activate
 
 python $PROJECT_DIR/src/models_classical/train.py \
-    --save_dir_name classical_results_interim \
+    --save_dir_name classical_results_interim_2022-07-14 \
     --path_data_dir $SCRATCH_DATA_DIR \
     --path_emb_dir $SCRATCH_DATA_DIR/processed/embeddings \
-    --emb_file_name df_embeddings_2022-07-11.pkl \
+    --emb_file_name df_embeddings_2022-07-14.pkl \
     --rand_search_iter 8000
 
